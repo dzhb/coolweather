3541552b39a1fded06050b2a1b06b40e56ca060d
dzhb <1241895026@qq.com>
2018-02-27 12:51:49 +0800

    完成遍历省市县三级列表的功能。

--- app/src/main/java/com/example/kkk/coolweather/db/County.java
+++ app/src/main/java/com/example/kkk/coolweather/db/County.java
@@ -6,7 +6,7 @@ import org.litepal.crud.DataSupport;
  * Created by k on 2018/2/25.
  */
 
-public class Country extends DataSupport {
+public class County extends DataSupport {
 
     private int id;
 
